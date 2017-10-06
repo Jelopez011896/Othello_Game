1dc9c104220b601803b7a4c7db2cd26e03988e54
Jelopez011896 <jesus@oomba.com>
2017-10-06 12:48:54 -0700

    Added button functionality

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,16 +1,37 @@
 class BoardController < ApplicationController
-  $game_board =
-  [
-    [2,2,2,2,2,2,2,2],
-    [2,2,2,2,2,2,2,2],
-    [2,2,2,2,2,2,2,2],
-    [2,2,2,0,1,2,2,2],
-    [2,2,2,1,0,2,2,2],
-    [2,2,2,2,2,2,2,2],
-    [2,2,2,2,2,2,2,2],
-    [2,2,2,2,2,2,2,2],
-  ]
-  def board1
+  def index
+    $game_board = game_board.new
+    $game_board =   [
+        [2,2,2,2,2,2,2,2],
+        [2,2,2,2,2,2,2,2],
+        [2,2,2,2,2,2,2,2],
+        [2,2,2,0,1,2,2,2],
+        [2,2,2,1,0,2,2,2],
+        [2,2,2,2,2,2,2,2],
+        [2,2,2,2,2,2,2,2],
+        [2,2,2,2,2,2,2,2],
+      ]
+  end
+
+  def new
+    @board1 = board1.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    $game_board[1][1]=2
+    puts "Ye boi"
+    redirect_to '/board1'
+  end
+  def destroy
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'board#board1'
+  root 'board#home'
   get '/board1',to:'board#board1'
+  post '/board1',to:'board#board1'
+  post 'update', to:'board#update'
 end
