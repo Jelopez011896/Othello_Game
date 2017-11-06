1c34c0ac69fe3a97d8ed2462013c08cebaaa4fc9
Jelopez011896 <jesus@oomba.com>
2017-11-06 09:01:29 -0800

    Changed how buttons interact with the Othello Gameboard logic

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,35 +1,14 @@
 class BoardController < ApplicationController
-  def index
-    $game_board = game_board.new
-    $game_board =   [
-        [2,2,2,2,2,2,2,2],
-        [2,2,2,2,2,2,2,2],
-        [2,2,2,2,2,2,2,2],
-        [2,2,2,0,1,2,2,2],
-        [2,2,2,1,0,2,2,2],
-        [2,2,2,2,2,2,2,2],
-        [2,2,2,2,2,2,2,2],
-        [2,2,2,2,2,2,2,2],
-      ]
-  end
+    load 'othello_board.rb'
 
-  def new
-    @board1 = board1.new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def show
+  def board
+    @board = OthelloBoard.new
+    @game1 = @board.new_board
   end
 
   def update
-    $game_board[1][1]=2
     puts "Ye boi"
-    redirect_to '/board1'
+    redirect_to '/board'
   end
   def destroy
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'board#home'
-  get '/board1',to:'board#board1'
-  post '/board1',to:'board#board1'
-  post 'update', to:'board#update'
+  get '/board',to:'board#board'
+  post '/board',to:'board#board'
+  post '/update',to:'board#update'
+  patch 'update', to:'board#update'
 end
