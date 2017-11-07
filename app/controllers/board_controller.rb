c2751857105ace816ea95c08618c3b84dc7a6ce5
Jelopez011896 <jesus@oomba.com>
2017-11-06 20:37:58 -0800

    Buttons are now properly tracked by presses in the game logic

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -11,12 +11,11 @@ class BoardController < ApplicationController
   end
 
   def update
-    puts $game1[0][0]
     @piece_place = params
     $board.place_piece(@piece_place['placement_id'][0].to_i,@piece_place['placement_id'][1].to_i)
-    puts $game1[0][0]
     redirect_to '/board'
   end
+
   def destroy
 
   end
--- app/helpers/othello_board.rb
+++ app/helpers/othello_board.rb
@@ -2,6 +2,8 @@
 class OthelloBoard
   def initialize
     $game_board
+    $player_turn = "white"
+
 
   end
 
